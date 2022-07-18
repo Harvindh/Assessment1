4c627fc822924ed2f1066b69084955875185c73d
Harvindh Srinivasan <harvindh.s@prodapt.com>
2022-07-18 17:37:53 +0530

    Source code

---
+++ SchoolRecord.java
@@ -0,0 +1,16 @@
+package com.prodapt.OOPbasics;
+
+public class SchoolRecord {
+
+	int schoolId;
+	String schoolName;
+	String schoolAddress;
+	void addRecords(int schoolId, String schoolName, String schoolAddress) {
+	System.out.println(schoolId + " " + " " + schoolName + " " + schoolAddress);
+	}
+	void displayRecords() {
+	System.out.println("Records of Schools in Chennai");
+	}
+	}
+
+
